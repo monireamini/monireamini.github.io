9638d586ecc25639738c8150c989a3db01ee9849
Monire Amini <monireamini@gmail.com>
2022-10-31 22:27:08 +0330

    fix ssr issue on github pages

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 import {apiRequest} from "../helpers/api-request";
 import ContainerBlock from "../components/container-block";
+import {useEffect} from "react";
 
 export default function Home() {
+
+    async function handleFetchRepos() {
+        const token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
+        const username = process.env.NEXT_PUBLIC_GITHUB_USER_NAME;
+
+        const res = await apiRequest({
+            url: `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
+            config: {headers: {Authorization: `token ${token}`}}
+        })
+        const repositories = res?.data?.items;
+    }
+
+    useEffect(() => {
+        handleFetchRepos();
+    }, []);
+
     return (
         <ContainerBlock>
             <p>Hi</p>
         </ContainerBlock>
     )
 }
-
-export const getServerSideProps = async () => {
-    const token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
-    const username = process.env.NEXT_PUBLIC_GITHUB_USER_NAME;
-
-    const res = await apiRequest({
-        url: `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
-        config: {headers: {Authorization: `token ${token}`}}
-    })
-    const repositories = res?.data?.items;
-
-    return {
-        props: {
-            repositories,
-        },
-    };
-};
