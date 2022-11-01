547601a68c09faa64688960a887beac93eabacc6
Monire Amini <monireamini@gmail.com>
2022-11-01 11:01:18 +0330

    update footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,32 +4,30 @@ import Contacts from "../contacts/contacts";
 const Footer = () => {
     return (
         <div className='bg-[#F1F1F1] dark:bg-gray-900'>
-            <div className='max-w-6xl mx-auto px-4 py-10 md:py-20'>
+            <div className='max-w-6xl mx-auto px-4 py-10 md:py-10'>
                 {/* footer horizontal line separator */}
                 <div className="h-0.5 w-full bg-white dark:bg-gray-700"/>
 
                 {/* footer content */}
-                <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
-                    <div>
-                        <p>&copy; Monireh. All Rights Reserved.</p>
-                    </div>
+                <div className="flex flex-row space-y-4 md:space-y-0 justify-center md:items-center mt-8">
                     <div>
                         <div>
-                            Developed by
+                            &copy;
                             <div
                                 className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-100">
                                 <a
-                                    className="hover:bg-green-500 rounded-md px-2 py-1  hover:text-gray-50"
+                                    className="hover:bg-green-500 rounded-md px-1 py-1  hover:text-gray-50"
                                     href="https://monireamini.github.io"
                                 >
-                                    Monireh Amini
+                                    Monireh.
                                 </a>
                             </div>
+                             All Rights Reserved.
                         </div>
                     </div>
-                    <div className="space-x-4 flex flex-row items-center">
-                        <Contacts/>
-                    </div>
+                    {/*<div className="space-x-4 flex flex-row items-center">*/}
+                    {/*    <Contacts/>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
