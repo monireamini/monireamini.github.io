56d689507dda51a3372e1c8d5cbc5e30649f9721
monireamini <40155832+monireamini@users.noreply.github.com>
2022-11-01 10:09:38 +0000

    Deploying to gh-pages from @ monireamini/monireamini.github.io@1c112243b8719a697f1757f95dd2587c69d5aab4 🚀

---
+++ _next/static/yYaS8lNP_cAn74P3pCn8I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/294-7b9ed056013d17fd.js","static/chunks/pages/index-87c656e386cc60d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
