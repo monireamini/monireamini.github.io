f713ca90540221dc48b1b6b086705f58e4474df8
Monire Amini <monireamini@gmail.com>
2022-11-01 15:29:25 +0330

    add email to contacts

--- data/social-links.ts
+++ data/social-links.ts
@@ -2,6 +2,7 @@ const socialLinks = {
     linkedin: 'https://www.linkedin.com/in/monirehamini/',
     github: 'https://github.com/monireamini',
     instagram: 'https://www.instagram.com/mineroworld/',
+    gmail: 'mailto:monireamini@gmail.com'
 }
 
 export default socialLinks;
