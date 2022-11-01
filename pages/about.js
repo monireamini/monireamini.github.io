6723b43c112c0b81baf143854bc6f88ad8fea252
Monire Amini <monireamini@gmail.com>
2022-11-01 18:42:18 +0330

    add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainLayout from "../components/main-layout/main-layout";
+import AboutMe from "../components/about-me/about-me";
+
+export default function about() {
+  return (
+    <MainLayout>
+      <AboutMe/>
+    </MainLayout>
+  );
+}
