0d551fd2a78fb025803c7987d4e81205a22ecce3
Monire Amini <monireamini@gmail.com>
2022-11-01 15:00:13 +0330

    add section header component

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -1,39 +1,13 @@
 import React from 'react';
-import Link from "next/link";
 import projects from "../../data/projects";
 import ProjectItem from "./project-item";
+import SectionHeader from "../section-header/section-header";
 
 const Projects = () => {
     return (
         <div className="bg-[#F1F1F1] dark:bg-gray-900">
             <div className="max-w-6xl mx-auto">
-                {/* title and see all button */}
-                <header className="flex flex-col md:flex-row justify-between items-center py-10 mx-10 md:my-20 lg:my-0">
-                    <h1 className="text-4xl lg:text-6xl max-w-lg font-bold text-gray-500 mb-10 md:my-0 md:text-white dark:text-gray-600 text-center">
-                        Projects
-                    </h1>
-                    <Link
-                        href="/projects"
-                        className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-arrow-up-right-square"
-                            stroke="4"
-                            strokeWidth="4"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
-                            />
-                        </svg>
-                        <p>View all</p>
-                    </Link>
-                </header>
+                <SectionHeader title={'Projects'} buttonLabel={'View all'} linkPath={'/projects'}/>
 
                 {/* items */}
                 <div className="grid md:grid-cols-3 gap-8">
--- components/repositories/repositories.tsx
+++ components/repositories/repositories.tsx
@@ -1,24 +1,33 @@
 import React, {useEffect} from 'react';
 import {apiRequest} from "../../helpers/api-request";
+import SectionHeader from "../section-header/section-header";
 
 const Repositories = () => {
 
-  async function handleFetchRepos() {
-    const token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
-    const username = process.env.NEXT_PUBLIC_GITHUB_USER_NAME;
+    async function handleFetchRepos() {
+        const token = process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN;
+        const username = process.env.NEXT_PUBLIC_GITHUB_USER_NAME;
 
-    const res = await apiRequest({
-      url: `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
-      config: {headers: {Authorization: `token ${token}`}}
-    })
-    const repositories = res?.data?.items;
-  }
+        const res = await apiRequest({
+            url: `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
+            config: {headers: {Authorization: `token ${token}`}}
+        })
+        const repositories = res?.data?.items;
+    }
 
-  useEffect(() => {
-    handleFetchRepos();
-  }, []);
+    useEffect(() => {
+        handleFetchRepos();
+    }, []);
 
-  return <p>repos</p>;
+    return (
+        <div className="bg-[#F1F1F1] dark:bg-gray-900">
+            <div className="max-w-6xl mx-auto">
+                <SectionHeader title={'Latest code'} buttonLabel={'View GitHub'} linkPath={`https://github.com/monireamini`}/>
+
+                <p>repos</p>
+            </div>
+        </div>
+    )
 };
 
 export default Repositories;
