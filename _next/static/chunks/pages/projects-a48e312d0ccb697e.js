cd96c087a83db14ab001f87115cf26ae262bb407
monireamini <40155832+monireamini@users.noreply.github.com>
2022-11-01 18:31:38 +0000

    Deploying to gh-pages from @ monireamini/monireamini.github.io@74d78598e8102b67c2655d5a8f538b09668f282e 🚀

---
+++ _next/static/chunks/pages/projects-a48e312d0ccb697e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{7670:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects",function(){return u(9587)}])},9587:function(n,t,u){"use strict";u.r(t);var e=u(5893);u(7294);let _=()=>(0,e.jsx)("p",{children:"Projects"});t.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7670)}),_N_E=n.O()}]);
